6ba62b5305e79cfebcec8a3dcc183faf997706d6
Ben Alpha <benalpha1105@gmail.com>
2020-06-22 04:40:06 +0700

    add user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,13 +1,47 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var UserSchema = new Schema({
-    name:{
-        type:String,
-        require:true
-    },
-    age:{
-        type:Number,
-        required:true
-    }
-})
-module.exports = mongoose.model("User",UserSchema,"User");
+var userSchema = new Schema({
+    phone: String,
+    password: String,
+    dob: Date,
+    fullname: String,
+    email: String,
+    address: String,
+    avatar: String,
+    gender: Boolean,
+    friends: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+            status: Number,
+            enums: [
+                0,    //'add friend',
+                1,    //'requested',
+                2,    //'pending',
+                3,    //'friends'
+            ]
+        }
+    ],
+    restaurants: [
+        {
+            restaurant: {
+                type: Schema.Types.ObjectId,
+                ref: "Restaurant"
+            }
+        }
+    ],
+    comments: [
+        {
+            idNewFeed: String,
+            content: String,
+            created_date: Date
+        }
+    ],
+    newfeeds:[{type:Schema.Types.ObjectId, ref:"NewFeed"}],
+    followers:[{type:Schema.Types.ObjectId,ref:"User"}],
+    following:[{type:Schema.Types.ObjectId,ref:"User"}],
+    active:Boolean
+}, { timestamps: true })
+module.exports = mongoose.model("User", userSchema, "User");

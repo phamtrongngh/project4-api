9ce303ff01649a3b5853eff0508eba27ff94d5cf
letuyettrinh10c12 <letuyettrinh10c12@gmail.com>
2020-07-03 16:45:35 +0700

    Fixed get 1 Product route

--- routes/product.route.js
+++ routes/product.route.js
@@ -3,7 +3,7 @@ let router = express.Router();
 let productController = require("../controllers/product.controller");
 
 router.get("/",productController.getProducts);
-router.get("/:id", productController.getProducts);
+router.get("/:id", productController.getProduct);
 router.post("/",productController.createProduct);
 router.put("/",productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);

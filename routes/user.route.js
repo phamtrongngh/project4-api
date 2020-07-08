d46e6cc70e31552b5abbc6d3aeced09d99576681
Minh Quang <letuyettrinh10c12@gmail.com>
2020-07-08 15:29:02 +0700

    route user modify

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,4 +2,5 @@ let express = require("express");
 let router = express.Router();
 let userController = require("../controllers/user.controller.js");
 router.get("/",userController.getUser);
+router.put("/",userController.updateUser);
 module.exports = router;

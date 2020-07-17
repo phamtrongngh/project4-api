537a3bcd5fb140547a49a1ef197d5ec231628804
Ben Alpha <benalpha1105@gmail.com>
2020-07-18 00:09:25 +0700

    add image field for FoodCategory Model

--- models/foodCategory.model.js
+++ models/foodCategory.model.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 var foodCategorySchema = new Schema({
-    name: String
+    name: String,
+    image: String
 }, { timestamps: true })
 module.exports = mongoose.model("FoodCategory", foodCategorySchema, "FoodCategory");

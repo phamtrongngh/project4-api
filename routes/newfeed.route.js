e32fcea2ab931669a67c51daef4d93d99082865d
letuyettrinh10c12 <letuyettrinh10c12@gmail.com>
2020-07-17 20:39:37 +0700

    Upload image testing

--- routes/newfeed.route.js
+++ routes/newfeed.route.js
@@ -1,10 +1,31 @@
 let express = require("express");
 let router = express.Router();
 let newfeedController = require("../controllers/newfeed.controller");
+var multer  = require('multer')
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/image')
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname)
+    }
+});
+let fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpg' || file.mimetype === 'image/jpg'){
+        cb(null, true)
+    }
+    else {cb(null, false)}  
+};
+
+let upload = multer({ storage: storage, limits: {
+    fileSize: 1024 * 1024 * 5
+}, fileFilter});
+let newfeedUpload = upload.single('images');
 
 router.get("/",newfeedController.getNewfeeds);
 router.get("/:id", newfeedController.getNewfeed);
-router.post("/",newfeedController.createNewfeed);
+router.post("/", newfeedUpload, newfeedController.createNewfeed);
 router.put("/",newfeedController.updateNewfeed);
 router.delete("/:id", newfeedController.deleteNewfeed);
 
--- routes/product.route.js
+++ routes/product.route.js
@@ -2,6 +2,7 @@ let express = require("express");
 let router = express.Router();
 let productController = require("../controllers/product.controller");
 
+
 router.get("/",productController.getProducts);
 router.get("/:id", productController.getProduct);
 router.post("/",productController.createProduct);

8b265907680efdc4a783e879bbebfb0e78c07437
Ben Alpha <benalpha1105@gmail.com>
2020-07-28 14:48:31 +0700

    cart-button

--- routes/restaurent.route.js
+++ routes/restaurent.route.js
@@ -7,10 +7,10 @@ let upload = require("../controllers/upload.controller");
 router.get("/getMyRestaurants",restaurantController.getMyRestaurants);
 router.get("/manageMyRestaurant/:id",restaurantController.manageMyRestaurant);
 router.get("/",restaurantController.getRestaurants);
+router.get("/getMenu/:id",restaurantController.getMenu);
 router.get("/:id", restaurantController.getRestaurant);
 router.post("/",upload.any(), restaurantController.createRestaurant);
 router.put("/",restaurantController.updateRestaurant);
-
 router.delete("/:id", restaurantController.deleteRestaurant);
 
 module.exports = router;

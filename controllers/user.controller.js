8f4013e98916c55616348aca3da566ba058d1299
Ben Alpha <benalpha1105@gmail.com>
2020-08-08 04:26:43 +0700

    edit friend response get my user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -15,8 +15,8 @@ module.exports.getUser = async (req, res) => {
         if (err) res.json(res);
         if (!user) { return res.json('Cant Find') }
         else {
-            await user.populate("newfeeds", async (err, result) => {
-                await result.populate("newfeeds.restaurant newfeeds.comments", (err, doc) => {
+            await user.populate("newfeeds friends", async (err, result) => {
+                await result.populate("newfeeds.restaurant newfeeds.comments friends.user", (err, doc) => {
                     doc.populate("newfeeds.comments.reply newfeeds.comments.user",(err,result)=>{
                         result.populate("newfeeds.comments.reply.user",(err,result)=>{
                             return res.json(result);
